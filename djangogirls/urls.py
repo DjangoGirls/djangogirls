35076d5c330089638ebe51bc46b9df97f306ec3c
Ola Sitarska <ola@sitarska.com>
2014-08-01 13:01:16 +0200

    Fixed flatpages links

--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('core.urls')),
     url(r'', include('django.contrib.flatpages.urls')),
+    url(r'', include('core.urls')),
+
 )
