d4a1ba8ebd56d91c03640614d4b96055a1efd0f5
Ola Sitarska <ola@sitarska.com>
2014-09-07 12:09:17 +0200

    Added gallery

--- static/js/djangogirls.js
+++ static/js/djangogirls.js
@@ -1,22 +1,27 @@
-<!-- JQUERY FOR SLIDING NAVIGATION -->   
+
 $(document).ready(function() {
-  $('a[href*=#]').each(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname
-    && this.hash.replace(/#/,'') ) {
-      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-       if ($target) {
-         var targetOffset = $target.offset().top - 50;
 
-<!-- JQUERY CLICK FUNCTION REMOVE AND ADD CLASS "ACTIVE" + SCROLL TO THE #DIV -->   
-         $(this).click(function() {
+    $('a[href*=#]').each(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname
+        && this.hash.replace(/#/,'') ) {
+          var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+          var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+           if ($target) {
+             var targetOffset = $target.offset().top - 50;
 
-           $('html, body').animate({scrollTop: targetOffset}, 1000);
-           return false;
-         });
-      }
-    }
-  });
+    <!-- JQUERY CLICK FUNCTION REMOVE AND ADD CLASS "ACTIVE" + SCROLL TO THE #DIV -->
+             $(this).click(function() {
+
+               $('html, body').animate({scrollTop: targetOffset}, 1000);
+               return false;
+             });
+          }
+        }
+    });
+
+    $('.gallery').sss({
+        showNav: false
+    });
 
 });
