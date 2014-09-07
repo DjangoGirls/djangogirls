222740d1ddf37a74451d225b8755f6f72f322e38
Ola Sitarska <ola@sitarska.com>
2014-09-07 18:53:17 +0200

    Added photo credit

---
+++ core/migrations/0004_event_photo_credit.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0003_auto_20140907_1359'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='event',
+            name='photo_credit',
+            field=models.CharField(max_length=200, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
--- core/models.py
+++ core/models.py
@@ -64,6 +64,7 @@ class Event(models.Model):
     country = models.CharField(max_length=200, null=False, blank=False)
     latlng = models.CharField(max_length=30, null=True, blank=True)
     photo = models.ImageField(upload_to="event/cities/", null=True, blank=True, help_text="The best would be 356 x 210px")
+    photo_credit = models.CharField(max_length=200, null=True, blank=True)
     main_organizer = models.ForeignKey(User, null=True, blank=True, related_name="main_organizer")
     team = models.ManyToManyField(User, null=True, blank=True)
     is_on_homepage = models.BooleanField(default=False)
