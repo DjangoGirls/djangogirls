795d3d3219edd7fa8942b8678df8a00cbd826c2d
Aleksandra Sendecka <asendecka@hauru.eu>
2014-10-25 17:39:28 +0200

    Creative Common license added to the page.
    It is possible to add a link to the original photo.
    All photos have link (CC) to Creative Common Non Commercial share alike licence.

---
+++ core/migrations/0007_auto_20141025_1521.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0006_auto_20141014_0754'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='coach',
+            options={'ordering': ('?',), 'verbose_name_plural': 'Coaches'},
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='photo_link',
+            field=models.URLField(null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
@@ -117,10 +117,11 @@ AWS_HEADERS = {
 }
 AWS_QUERYSTRING_AUTH = False
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+#STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 STATIC_ROOT = 'staticfiles'
