eeddb6ada1a4d011c72c0a9337c81a4899d50f37
syndg <syndbe@gmail.com>
2014-11-10 01:53:10 +0200

    Updated djangogirls/urls.py. It's no longer needed to use admin.autodiscover().

--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 from django.views.generic.base import RedirectView
 
-from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
     # Redirect old links:
