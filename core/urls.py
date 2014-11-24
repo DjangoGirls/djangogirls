7c4e780b2eda7235b57b2025f653c6eee15c8e75
Baptiste Mispelon <bmispelon@gmail.com>
2014-11-24 15:00:42 +0100

    Added a link to the ical view.

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^events/$', views.events, name='events'),
+    url(r'^events/calendar.ics$', views.events_ical, name='icalendar'),
     url(r'^resources/$', views.resources, name='resources'),
     url(r'^organize/$', views.organize, name='organize'),
     url(r'^story/$', views.stories, name='stories'),
-    url(r'^calendar/$', views.events_ical, name='icalendar'),
 
     url(r'^(?P<city>[\w\d/]+)$', 'core.views.event', name='event'),
     url(r'^$', views.index, name='index'),
