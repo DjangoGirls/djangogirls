a6e4a77215e6e54b8a2ebaabe544cac93854eb5a
marysia <maria.lowas@wp.pl>
2015-04-12 14:36:07 +0200

    Adding a migration that creates a Reviewers group

---
+++ jobs/migrations/0032_auto_20150408_2022.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.contrib.auth.models import Group, Permission
+
+
+def create_reviewer_group(apps, schema_editor, with_create_permissions=True):
+    Group = apps.get_model("auth", "Group")
+    Permission = apps.get_model("auth", "Permission")
+    try:
+        perm1 = Permission.objects.get(
+            codename='change_job', content_type__app_label='jobs')
+        perm2 = Permission.objects.get(
+            codename='change_meetup', content_type__app_label='jobs')
+    except Permission.DoesNotExist:
+        if with_create_permissions:
+            # Manually run create_permissions
+            from django.contrib.auth.management import create_permissions
+
+            assert not getattr(apps, 'models_module', None)
+            apps.models_module = True
+            create_permissions(apps, verbosity=0)
+            apps.models_module = None
+            return create_reviewer_group(
+                apps, schema_editor, with_create_permissions=False)
+        else:
+            raise
+    reviewers = Group.objects.create(name="Reviewers")
+    reviewers.permissions.add(perm1, perm2)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('jobs', '0030_auto_20150406_1940'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_reviewer_group),
+    ]
