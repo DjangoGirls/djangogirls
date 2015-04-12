f489eff6011978365f85b1f046d44694a90a762d
ksiedlarek <ksiedlarek@nctz.net>
2015-04-12 17:02:10 +0200

    merged migrations

---
+++ jobs/migrations/0032_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('jobs', '0030_auto_20150406_1940'),
+        ('jobs', '0031_auto_20150412_1441'),
+    ]
+
+    operations = [
+    ]
