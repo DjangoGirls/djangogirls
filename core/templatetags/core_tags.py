d3198c145602fbe2982da510ebad41a6841a00ce
Tzu-ping Chung <uranusjr@gmail.com>
2015-07-09 18:29:13 +0800

    Make event menu capable of showing absolute URI

---
+++ core/templatetags/core_tags.py
@@ -0,0 +1,16 @@
+from six.moves.urllib.parse import urlparse
+
+from django.template import Library
+
+
+register = Library()
+
+
+@register.simple_tag
+def build_menu_item_url(menu_url, event_page_url):
+    parse_result = urlparse(menu_url)
+    if parse_result.netloc:     # A full URI with domain.
+        return menu_url
+    elif parse_result.path.startswith('/'):     # Absolute path.
+        return menu_url
+    return '/{}/{}'.format(event_page_url, menu_url)
