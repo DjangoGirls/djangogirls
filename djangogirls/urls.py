f886bd2815ba69603195a2c24e10defb708c3921
Anna Warzecha <anna.warzecha@gmail.com>
2015-11-01 19:10:11 +0100

    Update ckeditor to new (backwards incompatible version)

--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -20,6 +20,5 @@ urlpatterns = patterns('',
     url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^account/', include('django.contrib.auth.urls')),
     url(r'', include('applications.urls', namespace='applications')),
-    url(r'', include('core.urls', namespace='core')),
-    url(r'^ckeditor/', include('ckeditor.urls')),
+    url(r'', include('core.urls', namespace='core'))
 )
