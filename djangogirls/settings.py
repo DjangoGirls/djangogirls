aa7d030e2d5a6c05d52cfde5240f158a1e3d2241
Ola Sitarska <ola@sitarska.com>
2015-12-04 16:15:18 +0000

    Run postgres on Travis

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'djangogirls.wsgi.application'
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(
-    default='postgres://djangogirls:@localhost:5432/djangogirls')
+    default='postgres://postgres:@localhost:5432/djangogirls')
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
