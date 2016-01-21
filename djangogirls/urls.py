4fd2593deccfe9c34d0654f66783b2b1beda5d5b
patjouk <lucie.daeye@gmail.com>
2016-01-21 22:42:17 +0100

    Adding captcha to application form

--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^account/', include('django.contrib.auth.urls')),
     url(r'', include('applications.urls', namespace='applications')),
-    url(r'', include('core.urls', namespace='core'))
+    url(r'^captcha/', include('captcha.urls')),
+    url(r'', include('core.urls', namespace='core')),
 ]
