ff1620f6d356e5b2b8e2bf5340fffdc0f4b0b666
patjouk <lucie.daeye@gmail.com>
2016-05-19 10:08:55 +0200

    adding todos

--- core/management/commands/prepare_dispatch.py
+++ core/management/commands/prepare_dispatch.py
@@ -8,6 +8,16 @@ class Command(BaseCommand):
     help = 'Generate "Next events" section for the Dispatch.'
 
     def handle(self, *args, **options):
+        print("PREVIOUS EVENTS")
+        #TODO: print a list of previous events since the last dispatch with links to eventpage. Ex: Nine events happended since the last dispatch: a, b, c...
+
+        print("NEXT EVENTS")
+        '''TODO: print a list of the next events listed by month, with links to eventpage: ex
+        June: a
+        July: b, c
+        '''
+
+        print("OPEN REGISTRATION")
         now = timezone.now()
         open_events = Event.objects.all().filter(eventpage__form__open_from__lte=now, eventpage__form__open_until__gte=now)
         result = []
@@ -18,4 +28,4 @@ class Command(BaseCommand):
             html = "<a href='https://djangogirls.org/%s'>%s</a>"%(url, city)
             result.append(html)
 
-        print(", ".join(result))
+        print("Registrations are still open for: " + ", ".join(result))
