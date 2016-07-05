efb4b2c569887c200a7d938d1004182cbe7bfad7
patjouk <lucie.daeye@gmail.com>
2016-07-05 12:21:28 +0200

    removing secrets file and using postactivate file

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -1,15 +1,8 @@
 import os
 import dj_database_url
-import json
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-try:
-    secret_path = os.path.join(BASE_DIR, "djangogirls", "secrets.json")
-    with open(secret_path) as f:
-        SECRETS = json.load(f)
-except IOError:
-    SECRETS = {}
 
 DEBUG = os.getenv('DJANGO_DEBUG') != 'FALSE'
 
@@ -179,15 +172,15 @@ EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND",
 DEFAULT_FROM_EMAIL = "hello@djangogirls.org"
 
 SLACK_API_KEY = os.environ.get('SLACK_API_KEY')
-TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
 
 RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
 RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
 # Using new No Captcha reCaptcha with SSL
 NOCAPTCHA = True
 
-STORE_DISCOUNT_CODE = SECRETS.get('STORE_DISCOUNT_CODE', None)
-TRELLO_API_KEY = SECRETS.get('TRELLO_API_KEY', None)
+STORE_DISCOUNT_CODE = os.environ.get('STORE_DISCOUNT_CODE')
+
+TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
 
 NOSE_ARGS = []
 
