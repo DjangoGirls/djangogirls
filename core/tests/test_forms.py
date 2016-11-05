127efd408e664262a63e66f491829704f04b63a9
Emad Mokhtar <emad.m.habib@gmail.com>
2016-11-05 14:28:29 +0100

    Improve forms tests

--- core/tests/test_forms.py
+++ core/tests/test_forms.py
@@ -11,22 +11,24 @@ class TestAddOrganizerForm(TestCase):
         self.event = Event.objects.create(name='Test',
                                           city='Test',
                                           country='Test')
-        self.user = User.objects.create_user('test')
+        # self.user = User.objects.create_user('test', password='SecretPassword')
 
     def test_notify_new_user(self):
-        form = AddOrganizerForm({'event': str(self.event),
-                                 'organizer': 'Test',
-                                 'email': 'test@test.com'})
-        if form.is_valid():
-            form.notify_new_user(self.user)
+        form = AddOrganizerForm(data={'event': self.event,
+                                      'organizer': 'test',
+                                      'email': 'test@test.com'})
+        if(form.is_valid()):
+            user = form.save()
+            form.notify_new_user(user)
             self.assertEqual(len(mail.outbox), 1)
             self.assertEqual(mail.outbox[0].content_type, 'text/html')
 
     def test_notify_exisiting_user(self):
-        form = AddOrganizerForm({'event': str(self.event),
+        form = AddOrganizerForm({'event': self.event,
                                  'organizer': 'Test',
                                  'email': 'test@test.com'})
-        if form.is_valid():
-            form.notify_existing_user(self.user)
+        if(form.is_valid()):
+            user = form.save()
+            form.notify_existing_user(user)
             self.assertEqual(len(mail.outbox), 1)
             self.assertEqual(mail.outbox[0].content_type, 'text/html')
