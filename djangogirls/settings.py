3f2f2d5b8cf9857231c511264de8fabec7184d40
Anna Warzecha <anna.warzecha@gmail.com>
2017-01-06 21:12:08 +0100

    Update requirements once again. This time properly

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -48,12 +48,13 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'djangogirls.urls'
