ae51a0021799150b70cefc1dbc5838c34935df82
Ola Sendecka <asendecka@google.com>
2017-01-07 18:49:56 +0000

    Move add_default_content and add_default_menu to Event model
    * refactor create_event & form to use these methods

---
+++ organize/create_event.py
@@ -0,0 +1,30 @@
+
+from core.models import Event
+
+
+def create_event_from_event_application(event_application):
+    """ Creates event based on the data from the object.
+        If the event has previous_event - we are copying data from the
+        existing old event for the given city.
+    """
+    name = 'Django Girls {}'.format(event_application.city)
+    email = '{}@djangogirls.org'.format(event_application.website_slug)
+
+    # TODO: take care of copying the event
+
+    event = Event.objects.create(
+        date=event_application.date,
+        city=event_application.city,
+        country=event_application.country,
+        latlng=event_application.latlng,
+        page_url=event_application.website_slug,
+        name=name,
+        page_title=name,
+        email=email,
+    )
+
+    # populate content & menu from the default event
+    event.add_default_content()
+    event.add_default_menu()
+
+    return event
