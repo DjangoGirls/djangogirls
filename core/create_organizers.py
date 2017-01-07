de4cbe5926de6777a45690d1493bf900d45927d0
Ola Sendecka <ola@hauru.eu>
2017-01-07 21:26:19 +0000

    Extract creation of one organizer to separate method.
    Ticket #342

--- core/create_organizers.py
+++ core/create_organizers.py
@@ -1,27 +1,38 @@
 from .models import User
 
 
-def create_organizers(event, event_application, email_password):
+def add_organizer(organizer, event):
     """
-        Create or get User objects based on EventApplication's
+        Add organizer to the event.
+
+        TODO: we need to think if create_organizers and create_events
+        are the best place for these logic. Maybe we should move it back to
+        the models.
+    """
+    defaults = {
+        "first_name": organizer.first_name,
+        "last_name": organizer.last_name,
+        "is_staff": True,
+        "is_active": True
+    }
+    user, created = User.objects.get_or_create(
+        email=organizer.email,
+        defaults=defaults
+    )
+    if created:
+        password = user.generate_password()
+        user.add_to_organizers_group()
+
+    event.invite_organizer_to_team(user, created, password)
+    return user
+
+
+def create_organizers(event, event_application, email_password):
+    """ Create or get User objects based on EventApplication's Coorganizers
     """
     organizers = []
     for organizer in event_application.coorganizers.all():
-        defaults = {
-            "first_name": organizer.first_name,
-            "last_name": organizer.last_name,
-            "is_staff": True,
-            "is_active": True
-        }
-        user, created = User.objects.get_or_create(
-            email=organizer.email,
-            defaults=defaults
-        )
-        if created:
-            password = user.generate_password()
-            user.add_to_organizers_group()
-
-        event.invite_organizer_to_team(user, created, password)
+        user = add_organizer(organizer, event)
         organizers.append(user)
 
     return organizers
