5af1656d0e3b0767bbca9dfd41ca9f5231c4e276
Ola Sendecka <asendecka@google.com>
2017-01-07 19:07:02 +0000

    Refactor `create_event` to use `add_default` from Event
    Ticket #342

--- core/create_event.py
+++ core/create_event.py
@@ -7,30 +7,6 @@ from core.default_eventpage_content import (
 from core.models import Event
 
 
-# TODO: Probably this is not the best place for these two functions.
-# Make sure to rethink it once again and refactor core.forms to use
-# them, so we do not repeat the code and we preserve ability to create
-# Event through Django admin
-def _add_default_content(event):
-    """Populate EventPageContent with default layout"""
-    data = get_default_eventpage_data()
-
-    for i, section in enumerate(data):
-        section['position'] = i
-        section['content'] = render_to_string(section['template'])
-        del section['template']
-        event.content.create(**section)
-
-
-def _add_default_menu(event):
-    """Populate EventPageMenu with default links"""
-    data = get_default_menu()
-
-    for i, link in enumerate(data):
-        link['position'] = i
-        event.menu.create(**link)
-
-
 def create_event_from_event_application(event_application):
     """ Creates event based on the data from the object.
         If the event has previous_event - we are copying data from the
@@ -53,7 +29,7 @@ def create_event_from_event_application(event_application):
     )
 
     # populate content & menu from the default event
-    _add_default_content(event)
-    _add_default_menu(event)
+    event.add_default_content()
+    event.adefault_menu()
 
     return event
