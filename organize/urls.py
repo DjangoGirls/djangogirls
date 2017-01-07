75987f8542aa64f29ced70e557c047bc92be6301
Anna Warzecha <anna.warzecha@gmail.com>
2017-01-07 19:29:18 +0100

    Remove prev & next steps from the form, add proper link to form steps

--- organize/urls.py
+++ organize/urls.py
@@ -2,10 +2,16 @@ from django.conf.urls import url
 
 from . import views
 
+organize_form_wizard = views.OrganizeFormWizard.as_view(
+    views.FORMS,
+    url_name='organize:form_step',
+    done_step_name='finished')
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^prerequisites/$', views.prerequisites, name='prerequisites'),
     url(r'^commitment/$', views.commitment, name='commitment'),
-    url(r'^form/$', views.OrganizeFormWizard.as_view(), name='form'),
+    url(r'^form/(?P<step>.+)/$', organize_form_wizard, name='form_step'),
+    # url(r'^form/$', organize_form_wizard, name='form'),
     url(r'^form/thank_you/$', views.form_thank_you, name='form_thank_you'),
 ]
