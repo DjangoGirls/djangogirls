376e60c58cf9d0842aebcb03fd003095127fb777
Ola Sendecka <ola@hauru.eu>
2017-01-07 15:15:25 +0000

    Add status, main organizer name, comment and rejection to admin list view.
    Ticket #342

--- organize/admin.py
+++ organize/admin.py
@@ -10,5 +10,20 @@ class InlineCoorganizerAdmin(admin.TabularInline):
 
 @admin.register(EventApplication)
 class EventApplicationAdmin(admin.ModelAdmin):
-    list_display = ('city', 'country', 'date')
+    list_display = (
+        'city',
+        'country',
+        'date',
+        'main_organizer',
+        'status',
+        'comment',
+        'rejection_reason',
+    )
     inlines = (InlineCoorganizerAdmin,)
+
+    def main_organizer(self, obj):
+        return "{} {} ({})".format(
+            obj.main_organizer_first_name,
+            obj.main_organizer_last_name,
+            obj.main_organizer_email
+        )
