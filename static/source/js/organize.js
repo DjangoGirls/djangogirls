51f9f356890fbf6c3ff249478976af27306671d1
Michael Strutt <mstrutt@potatolondon.com>
2017-01-07 19:12:50 +0000

    Styling application step 2

--- static/source/js/organize.js
+++ static/source/js/organize.js
@@ -1,13 +1,17 @@
 $(function() {
     $('[data-required-form]').each(function() {
         var $this = $(this);
-        var $checkboxes = $this.find('[type="checkbox"]');
+        var $checkboxes = $this.find('input[type="checkbox"][required]');
+        var $textareas = $this.find('textarea[required]')
         var $submit = $this.find('[type="submit"]');
 
         var validate = function() {
             var allChecked = true;
             $checkboxes.each(function() {
                 allChecked = allChecked && $(this).is(':checked');
+            });
+            $textareas.each(function() {
+                allChecked = allChecked && $(this).val().length > 0
             });
             if (allChecked) {
                 $submit.removeAttr('disabled');
@@ -17,7 +21,7 @@ $(function() {
         }
 
         $this.attr('novalidate', 'novalidate');
-        $this.on('change', '[type="checkbox"]', validate);
+        $this.on('input, change', '[required]', validate);
         validate();
     });
 });
