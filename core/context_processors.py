4fe3775640e0b728584edf81d8fb2ec85724b455
Patryk Zawadzki <patrys@room-303.com>
2017-01-07 11:38:40 +0100

    Remove jobs and meetups

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,7 +1,5 @@
 from django.db.models import Sum
 
-from jobs.models import Meetup
-
 from .models import Event, Postmortem
 
 
@@ -9,8 +7,8 @@ def statistics(request):
     future_events = Event.objects.future()
     past_events = Event.objects.past()
     countries = Event.objects.values('country').distinct()
-    attendees = Postmortem.objects.all().aggregate(attendees=Sum('attendees_count'), applicants=Sum('applicants_count'))
-    meetup_count = Meetup.visible_objects.count()
+    attendees = Postmortem.objects.all().aggregate(
+        attendees=Sum('attendees_count'), applicants=Sum('applicants_count'))
 
     return {
         'past_events_count': past_events.count(),
@@ -18,6 +16,5 @@ def statistics(request):
         'all_events_count': past_events.count() + future_events.count(),
         'countries_count': countries.count(),
         'attendees_sum': attendees['attendees'],
-        'applicants_sum': attendees['applicants'],
-        'meetup_count': meetup_count
+        'applicants_sum': attendees['applicants']
     }
