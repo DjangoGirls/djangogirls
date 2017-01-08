f612e651bb1676781abf9558a6075df11f97a878
Baptiste Mispelon <bmispelon@gmail.com>
2017-01-08 13:42:39 +0100

    Added change_status_to() method on EventApplication querysets too

--- organize/admin.py
+++ organize/admin.py
@@ -14,12 +14,12 @@ class InlineCoorganizerAdmin(admin.TabularInline):
 
 
 def change_status_to_on_hold(modeladmin, request, queryset):
-    queryset.update(status=ON_HOLD, status_changed_at=timezone.now())
+    queryset.change_status_to(ON_HOLD)
 change_status_to_on_hold.short_description = "Move selected application to on hold"
 
 
 def change_status_to_in_review(modeladmin, request, queryset):
-    queryset.update(status=IN_REVIEW, status_changed_at=timezone.now())
+    queryset.change_status_to(IN_REVIEW)
 change_status_to_in_review.short_description = "Move selected application to in review"
 
 
