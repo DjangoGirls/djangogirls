dbb57e68fa724f92d624acbb2647187ffaa252d6
Ola Sitarska <ola@sitarska.com>
2017-01-08 12:50:31 +0000

    Move sending emails into core/emails.py

--- core/emails.py
+++ core/emails.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 
@@ -11,7 +12,7 @@ def notify_existing_user(user, event):
         'event': event
     })
     subject = 'You have been granted access to new Django Girls event'
-    send_email(content, subject, user)
+    send_email(content, subject, [user.email])
 
 
 def notify_new_user(user, event, password, errors=None):
@@ -25,13 +26,35 @@ def notify_new_user(user, event, password, errors=None):
         'errors': errors,
     })
     subject = 'Access to Django Girls website'
-    send_email(content, subject, user)
+    send_email(content, subject, [user.email])
 
 
-def send_email(content, subject, user):
+def send_application_rejection_email(event_application):
+    """ Sends a rejection email to all organizars who created this application
+    """
+    subject = "Application to organize Django Girls {} has been reviewed".format(event_application.city)
+    content = render_to_string('emails/organize/rejection.html', {
+        'application': event_application
+    })
+    send_email(content, subject, event_application.get_all_recipients())
+
+
+def send_application_deployed_email(event_application, event, email_password):
+    subject = "Congrats! Your application to organize Django Girls {} has been accepted!".format(
+        event_application.city)
+    content = render_to_string('emails/organize/event_deployed.html', {
+        'event': event,
+        'password': email_password,
+    })
+    recipients = event_application.get_all_recipients()
+    recipients.append(event.email)  # add event's djangogirls.org email
+    send_email(content, subject, recipients)
+
+
+def send_email(content, subject, recipients):
     msg = EmailMessage(subject,
                        content,
-                       "Django Girls <hello@djangogirls.org>",
-                       [user.email])
+                       settings.DEFAULT_FROM_EMAIL,
+                       recipients)
     msg.content_subtype = "html"
     msg.send()
