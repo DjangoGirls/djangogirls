a575a72a90fa74d01deab63f3132752c7c80ccac
Anna Warzecha <anna.warzecha@gmail.com>
2017-11-28 23:25:50 +0100

    Remove TestCase for tests in core.models

---
+++ tests/coach/test_models.py
@@ -0,0 +1,9 @@
+
+from coach.models import Coach, DEFAULT_COACH_PHOTO
+
+
+def test_default_photo():
+    assert Coach.objects.count() == 0
+    coach = Coach.objects.create(name="Test Test", twitter_handle="@test")
+    assert Coach.objects.count() == 1
+    assert coach.photo_url == DEFAULT_COACH_PHOTO
