c3af7b5bab55d7fce035fb3a36f3d53a8863ddd3
amakarudze <amakarudze@gmail.com>
2018-06-14 15:32:58 +0200

    Updated quotes.py with quotes from DG community

--- core/quotes.py
+++ core/quotes.py
@@ -1,6 +1,18 @@
 DONORS_QUOTES = [
-    {'text': 'Test', 'author': 'Test Author', 'url': 'http://test.com'},
-    {'text': 'Test1', 'author': 'Test Author 1', 'url': 'http://test1.com'},
-    {'text': 'Test2', 'author': 'Test Author 2', 'url': 'http://test2.com'},
-    {'text': 'Test3', 'author': 'Test Author 3', 'url': 'http://test3.com'},
+    {'text': 'Seek mentorship. A mentor will help you grow faster than you can imagine. ',
+     'author': 'Ana Balica',
+     'url': 'http://blog.djangogirls.org/post/124569551358/your-django-story-meet-ana-balica'},
+    {'text': 'Programming isn’t just for geeks!', 'author': 'Caroline Ochieng',
+     'url': 'http://blog.djangogirls.org/post/170424127090/your-django-story-caroline-ochieng'},
+    {'text': 'I am proud of having changed my career and becoming a developer. It still feels a bit '
+             'unreal to me.', 'author': 'Päivi Suomela',
+     'url': 'http://blog.djangogirls.org/post/137758290583/your-django-story-meet-p%C3%A4ivi-suomela'},
+    {'text': 'Django Girls Lagos brought about the creation of a Nigerian Python community that '
+             'didn’t exist before. It brought together a lot of great minds who are passionate '
+             'about promoting the use of Python in Nigeria.',
+     'author': 'Aisha Bello',
+     'url': 'http://blog.djangogirls.org/post/138923168468/your-django-story-meet-aisha-bello'},
+    {'text': 'You don’t have know everything, you just have to be open to learning anything.',
+     'author': 'Heather Bryant',
+     'url': 'http://blog.djangogirls.org/post/137097700508/your-django-story-meet-heather-bryant'},
 ]
