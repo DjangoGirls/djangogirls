8bd34af038934aa01335b707a165f1ed6076c261
amakarudze <amakarudze@gmail.com>
2018-06-25 15:33:54 +0200

    Add more quotes

--- core/quotes.py
+++ core/quotes.py
@@ -2,8 +2,10 @@ DONORS_QUOTES = [
     {'text': 'Seek mentorship. A mentor will help you grow faster than you can imagine. ',
      'author': 'Ana Balica',
      'url': 'http://blog.djangogirls.org/post/124569551358/your-django-story-meet-ana-balica'},
-    {'text': 'Programming isn’t just for geeks!', 'author': 'Caroline Ochieng',
-     'url': 'http://blog.djangogirls.org/post/170424127090/your-django-story-caroline-ochieng'},
+    {'text': 'I love the fact that coding makes me work independently, exposes me to challenges'
+             ' and gives me a chance to keenly think about how to achieve the set objective.',
+     'author': 'Irene Nandutu',
+     'url': 'http://blog.djangogirls.org/post/141837521678/your-django-story-meet-irene-nandutu'},
     {'text': 'I am proud of having changed my career and becoming a developer. It still feels a bit '
              'unreal to me.', 'author': 'Päivi Suomela',
      'url': 'http://blog.djangogirls.org/post/137758290583/your-django-story-meet-p%C3%A4ivi-suomela'},
@@ -12,6 +14,25 @@ DONORS_QUOTES = [
              'about promoting the use of Python in Nigeria.',
      'author': 'Aisha Bello',
      'url': 'http://blog.djangogirls.org/post/138923168468/your-django-story-meet-aisha-bello'},
+    {'text': 'Django Girls KC 2016 was really a turning point for me. It gave me the skills and '
+             'confidence to learn as much as I could about programming. I accepted a volunteer '
+             'position as Curriculum Director for Coding & Cupcakes. I have also become an organizer '
+             'for the python user group PythonKC. My favourite accomplishment was returning to '
+             'Django Girls KC in 2017 as a mentor!',
+     'author': 'Muriel Green',
+     'url': 'http://blog.djangogirls.org/post/170349070483/your-django-story-meet-muriel-green'},
+    {'text': 'Don’t get scared, get started. Almost everything is doable by anyone. Most of the'
+             ' things are easy to learn. So start doing what you like. There are lot of things'
+             ' on the web to help you out. Just be truthful to yourself.',
+     'author': 'Tapasweni Pathak',
+     'url': 'http://blog.djangogirls.org/post/130542018183/your-django-story-meet-tapasweni-pathak'},
+    {'text': 'It became clearer to me how many women have interest in the tech field and how they '
+             'feel unable or di-stimulated to go after this interest because of the predominance '
+             'of men in the area. I believe this kind of work, like the one done by Django Girls, '
+             'is super important to empower women and, little by little, change the world. I have'
+             ' loved taking part of something so amazing.',
+     'author': 'Olga Pinheiro Garcia',
+     'url': 'http://blog.djangogirls.org/post/169817148415/django-girls-empowers-women'},
     {'text': 'You don’t have know everything, you just have to be open to learning anything.',
      'author': 'Heather Bryant',
      'url': 'http://blog.djangogirls.org/post/137097700508/your-django-story-meet-heather-bryant'},
