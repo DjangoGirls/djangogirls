24aceed5ba503b59a109552ef4da71b29afd6931
amakarudze <amakarudze@gmail.com>
2020-06-02 16:30:42 +0200

    Suspend applications to organise new events

--- organize/urls.py
+++ organize/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^prerequisites/$', views.prerequisites, name='prerequisites'),
     url(r'^commitment/$', views.commitment, name='commitment'),
     url(r'^form/thank_you/$', views.form_thank_you, name='form_thank_you'),
-    url(r'^form/(?P<step>.+)/$', views.organize_form_wizard, name='form_step')
+    url(r'^form/(?P<step>.+)/$', views.organize_form_wizard, name='form_step'),
+    url(r'^suspend/$', views.suspend, name='suspend')
 ]
