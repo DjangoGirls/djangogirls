d6b414b271d65df138387b3ec0406a38bd996392
amakarudze <amakarudze@gmail.com>
2020-06-02 16:51:05 +0200

    Fix urls conflicts

--- organize/urls.py
+++ organize/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^prerequisites/$', views.prerequisites, name='prerequisites'),
-    url(r'^commitment/$', views.commitment, name='commitment'),
-    url(r'^form/thank_you/$', views.form_thank_you, name='form_thank_you'),
-    url(r'^form/(?P<step>.+)/$', views.organize_form_wizard, name='form_step'),
-    url(r'^suspend/$', views.suspend, name='suspend')
+    path('', views.index, name='index'),
+    path('prerequisites/', views.prerequisites, name='prerequisites'),
+    path('commitment/', views.commitment, name='commitment'),
+    path('form/thank_you/', views.form_thank_you, name='form_thank_you'),
+    path('form/<slug:step>/', views.organize_form_wizard, name='form_step'),
+    path('suspend/', views.suspend, name='suspend')
 ]
