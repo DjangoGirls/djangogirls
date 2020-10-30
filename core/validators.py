67afe9d5abb861ab016013bd4ec7b79a014c3710
Anna Makarudze <amakarudze@gmail.com>
2020-10-30 14:22:57 +0200

    Clean up validate_event_date

--- core/validators.py
+++ core/validators.py
@@ -1,10 +1,10 @@
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 
 from django.core.exceptions import ValidationError
 
 
-def validate_approximatedate(date):
-    if date.month == 0:
+def validate_approximatedate(e_date):
+    if e_date.month == 0:
         raise ValidationError(
             'Event date can\'t be a year only. '
             'Please, provide at least a month and a year.'
@@ -13,8 +13,6 @@ def validate_approximatedate(date):
 
 def validate_event_date(e_date):
     today = date.today()
-    event_date = datetime.date(datetime.strptime('{0}-{1}-{2}'.format(e_date.year, e_date.month, e_date.day),
-                                                 '%Y-%m-%d'))
-    if event_date - today < timedelta(days=90):
+    if date(e_date.year, e_date.month, e_date.day) - today < timedelta(days=90):
         raise ValidationError('Your event date is too close. '
                               'Workshop date should be at least 3 months (90 days) from now.')
