5840fa47ea609fdaa9fc45aa01175cfc2ef55df9
Mark Walker <mark.walker@realbuzz.com>
2021-08-11 01:44:31 +0100

    Initial setup of donations
    Moved donate page into new donations app with stripe form.
    
    Removed old donations template
    
    Made donations view more defensive against errors and added basic tests
    
    Removed auto generated `tests.py` and added note in the `README.md` about `python-dotenv`
    
    Moved templates and added missing translation tags

---
+++ donations/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+
+class StripeForm(forms.Form):
+    """ Form for handling stripe donations """
+
+    DONATION_CHOICES = (
+        ('10', '10'),
+        ('25', '25'),
+        ('50', '50'),
+        ('100', '100'),
+    )
+    CURRENCY_CHOICES = (
+        ('usd', '$ (US dollars)'),
+        ('eur', '€ (Euro)'),
+        ('gbp', '£ (GBP)'),
+    )
+
+    currency = forms.ChoiceField(choices=CURRENCY_CHOICES)
+    amount = forms.ChoiceField(choices=DONATION_CHOICES)
+    email = forms.EmailField()
+    name = forms.CharField()
