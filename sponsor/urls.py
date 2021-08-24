c68738022f16bba660280a2bef6992f80e83b2d3
Mark Walker <mark.walker@realbuzz.com>
2021-08-25 00:57:06 +0100

    Comment out Sponsor URLs and View because they're not being loaded

--- sponsor/urls.py
+++ sponsor/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path
-from django.contrib.auth.decorators import login_required
-
-from sponsor.views import SponsorRequestView
-
-app_name = 'sponsor'
-urlpatterns = [
-    path(
-        'sponsor-request/', login_required(SponsorRequestView.as_view()),
-        name='sponsor-request'
-    ),
-]
+# from django.urls import path
+# from django.contrib.auth.decorators import login_required
+#
+# from sponsor.views import SponsorRequestView
+#
+# app_name = 'sponsor'
+# urlpatterns = [
+#     path(
+#         'sponsor-request/', login_required(SponsorRequestView.as_view()),
+#         name='sponsor-request'
+#     ),
+# ]
