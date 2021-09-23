0c7433901a7a1ce6292c733072b201d6aad5eda8
Ramon Saraiva <ramonsaraiva@gmail.com>
2021-09-23 20:46:32 -0300

    Add code plugin to tinymce widget

--- core/admin/forms/event_page_content.py
+++ core/admin/forms/event_page_content.py
@@ -1,12 +1,26 @@
+import copy
+
+import tinymce
+
 from django import forms
 from tinymce.widgets import AdminTinyMCE
+
+
+default_tinymce_config = copy.deepcopy(tinymce.settings.DEFAULT_CONFIG)
+default_tinymce_config['plugins'] += ' | code'
+default_tinymce_config['toolbar'] += ' | code'
 
 
 class EventPageContentForm(forms.ModelForm):
 
     class Meta:
         widgets = {
-            'content': AdminTinyMCE()
+            'content': AdminTinyMCE(
+                mce_attrs={
+                    'plugins': default_tinymce_config['plugins'],
+                    'toolbar': default_tinymce_config['toolbar'],
+                },
+            )
         }
         fields = (
             'event',
