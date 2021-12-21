b550bd95177b86a97f89828fe145af14fb76d955
Ramon Saraiva <ramonsaraiva@gmail.com>
2021-12-21 12:23:14 -0300

    Add admin action to load remote stories

--- story/admin.py
+++ story/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-from story.models import Story
+
+from .models import Story
+from .services import (
+    create_missing_stories,
+    request_latest_stories_from_tumblr,
+)
+
+
+def load_remote_stories(modeladmin, request, queryset):
+    latest_stories = request_latest_stories_from_tumblr()
+    create_missing_stories(list(latest_stories))
+
+
+load_remote_stories.short_description = _("Load remote stories")
 
 
 class StoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_story', 'created')
     search_fields = ('name', 'content')
     list_filter = ('is_story',)
+    actions = [
+        load_remote_stories,
+    ]
 
 
 admin.site.register(Story, StoryAdmin)
