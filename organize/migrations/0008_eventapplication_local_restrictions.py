c3a51abdb2219944744ec3db64973b5e205a79b3
Anna Makarudze <amakarudze@gmail.com>
2022-03-07 11:27:01 +0200

    Fix migration reference

--- organize/migrations/0008_eventapplication_local_restrictions.py
+++ organize/migrations/0008_eventapplication_local_restrictions.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('organize', '0007_auto_20210920_1053'),
+        ('organize', '0007_auto_20210906_1216'),
     ]
 
     operations = [
