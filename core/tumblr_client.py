7afd39e7e5011d8edc8a4140953bb5c3a68706a7
Ramon Saraiva <ramonsaraiva@gmail.com>
2022-05-05 09:04:11 -0300

    Move services to tumblr_client module and add management command to fetch tumblr stories

--- core/tumblr_client.py
+++ core/tumblr_client.py
@@ -4,14 +4,13 @@ import datetime
 from typing import (
     Any,
     Iterator,
+    Optional,
 )
 
 import requests
 from requests.exceptions import RequestException
 
 from django.conf import settings
-
-from .models import Story
 
 
 logger = logging.getLogger(__name__)
@@ -32,6 +31,21 @@ class RemoteStory:
         ]
 
     @property
+    def image_parts(self) -> list[dict]:
+        return [
+            content_part["media"]
+            for content_part in self.content_parts
+            if content_part["type"] == "image"
+        ]
+
+    @property
+    def banner_url(self) -> Optional[str]:
+        if not self.image_parts:
+            return None
+        # Each image part has multiple image sizes, we're picking the first
+        return self.image_parts[0][0]["url"]
+
+    @property
     def title(self) -> str:
         return self.text_parts[0]
 
@@ -44,7 +58,7 @@ class RemoteStory:
         return "Your Django Story" in self.title
 
 
-def parse_tumblr_posts(posts_data: list[dict[str, Any]]) -> Iterator[RemoteStory]:
+def parse_posts(posts_data: list[dict[str, Any]]) -> Iterator[RemoteStory]:
     for post_data in posts_data:
         yield RemoteStory(
             url=post_data["post_url"],
@@ -53,9 +67,9 @@ def parse_tumblr_posts(posts_data: list[dict[str, Any]]) -> Iterator[RemoteStory
         )
 
 
-def request_latest_stories_from_tumblr() -> Iterator[RemoteStory]:
-    # TODO: probably write a tumblr client if more endpoints
-    # get accessed in the future
+def request_latest_stories() -> Iterator[RemoteStory]:
+    # TODO: probably write a wrapper for tumblr requests if
+    # more endpoints are used in the future
     blog_base_url = (
         f"{settings.TUMBLR_API_BASE_URL}/blog/{settings.TUMBLR_BLOG_HOSTNAME}"
     )
@@ -74,31 +88,4 @@ def request_latest_stories_from_tumblr() -> Iterator[RemoteStory]:
         return iter(())
 
     posts_data = response.json()["response"]["posts"]
-    return parse_tumblr_posts(posts_data)
-
-
-def create_missing_stories(remote_stories: list[RemoteStory]) -> int:
-    created = 0
-    remote_urls = set(remote_story.url for remote_story in remote_stories)
-    in_db_urls = set(
-        Story.objects.filter(post_url__in=remote_urls).values_list(
-            "post_url", flat=True
-        )
-    )
-    to_create_urls = remote_urls.difference(in_db_urls)
-    missing_stories = [
-        remote_story
-        for remote_story in remote_stories
-        if remote_story.url in to_create_urls
-    ]
-    for missing_story in missing_stories:
-        # TODO: still missing image from story posts
-        story = Story.objects.create(
-            post_url=missing_story.url,
-            name=missing_story.title,
-            content=missing_story.content,
-            is_story=missing_story.is_story,
-        )
-        story.created = missing_story.created
-        story.save()
-    return created
+    return parse_posts(posts_data)
