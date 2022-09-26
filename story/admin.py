17244828b020d39810045289ecc0aea8c4db3917
Ramon Saraiva <ramonsaraiva@gmail.com>
2022-09-26 09:20:30 +0000

    Revert story admin changes

--- story/admin.py
+++ story/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 
 
-from .models import Story
-
-
 class StoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_story', 'created')
     search_fields = ('name', 'content')
